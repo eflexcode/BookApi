2ccd4e302ff629e2e40d3f040948a21b2db8cb05
Eze Larry Ifeanyi <ezelarry379@gmail.com>
2023-06-28 01:28:52 +0100

    entities set

--- src/main/java/com/larrex/bookapi/model/Author.java
+++ src/main/java/com/larrex/bookapi/model/Author.java
@@ -1,10 +1,18 @@
 package com.larrex.bookapi.model;
 
+import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
 
 @Data
+@Entity
 public class Author {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
@@ -13,4 +21,11 @@ public class Author {
 
     private String biography;
 
+    @CreationTimestamp
+    @Column(name = "created_at",nullable = false,updatable = false)
+    private Timestamp createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at",nullable = false,updatable = false)
+    private Timestamp updatedAt;
 }
--- src/main/java/com/larrex/bookapi/model/Book.java
+++ src/main/java/com/larrex/bookapi/model/Book.java
@@ -1,4 +1,51 @@
 package com.larrex.bookapi.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+@Data
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String imageUrl;
+
+    private String about;
+
+    private BigDecimal price;
+
+    private String language;
+
+    private String publisher;
+
+    private String publishedAt;
+
+    private String pageCount;
+
+    @CreationTimestamp
+    @Column(name = "created_at",nullable = false,updatable = false)
+    private Timestamp createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at",nullable = false,updatable = false)
+    private Timestamp updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY,optional = false)
+    @JoinColumn(name = "author_id",nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private Author author;
 }
